3267d78f118d70aad87d3ee085ccf9b8546dd5b5
paulo henrique <pereirapaulo436@gmail.com>
2023-03-07 17:50:24 -0300

    introducaojava

---
+++ src/semana03/Introducao.java
@@ -0,0 +1,42 @@
+package semana03;
+
+public class Introducao {
+
+    public static void main(string[] args){
+
+//tipo primitivos
+
+//inteiro
+//byte, short, int, long
+
+byte n1 = 100; //-128 a 127 - 1 byte
+short n2 = 3000; //-32768 a 32767 - 2 bytes
+int n3 = 43453453; //2 bilhões - 4 bytes
+long n4 = 53453452; //8 bytes
+
+//ponto flutuante
+
+float nota1 = 1.232f;
+double nota2 = 2.534252;
+
+//char
+
+char letra = 'a';
+
+//boolean
+
+boolean ligado = true;
+boolean teste = false;
+
+//string
+
+String nome = "paulo henrique";
+
+System.out.println(nome);
+System.out.println(nome.toLowerCase());
+System.out.println(nome.toUpperCase());
+
+
+
+    }
+}
